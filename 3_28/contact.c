8a60b9907f676cc7fe7571f9df7986f4cc3f5dcd
123 <123@LAPTOP-3MLMSRRE>
2026-03-28 15:05:10 +0800

    添加项目文件。

---
+++ 3_28/contact.c
@@ -0,0 +1,162 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include "contact.h"
+
+
+void InitContact(Contact* pc)
+{
+	pc->count = 0;
+	memset(pc->data, 0, sizeof(pc->data));
+}
+void AddContact(Contact* pc)
+{
+	assert(pc);
+	if (pc->count == 100)
+	{
+		printf("通讯录已满\n");
+		return;
+	}
+	printf("请输入姓名:>");
+	scanf("%s", pc->data[pc->count].name);
+	printf("请输入年龄:>");
+	scanf("%d", &(pc->data[pc->count].age));
+	printf("请输入性别:>");
+	scanf("%s", pc->data[pc->count].sex);		
+	printf("请输入电话:>");
+	scanf("%s", pc->data[pc->count].tele);
+	printf("请输入地址:>");
+	scanf("%s", pc->data[pc->count].addr);
+
+	pc->count++;
+	printf("增加成功\n");
+}
+
+void ShowContact(const Contact* pc)
+{
+	assert(pc);
+	int i = 0;
+	
+	for (i = 0;i < pc->count;i++)
+	{
+		printf("%-20s\t%-3s\t%-5s\t%-12s\t%-30s\n","姓名","年龄", "性别","电话","地址");
+
+		printf("%-20s\t%-3d\t%-5s\t%-12s\t%-30s\n", pc->data[i].name,
+			                                   pc->data[i].age,
+		                                   	   pc->data[i].sex,
+			                                   pc->data[i].tele,
+			                                   pc->data[i].addr);
+	}
+
+}
+int FindByName(Contact* pc, char name[20])
+{
+	int i = 0;
+	for (i = 0;i < pc->count;i++)
+	{
+		if (strcmp(pc->data[i].name, name) == 0)
+		{
+			return i;
+		}
+		
+	}
+	return -1;
+}
+void DelContact(Contact* pc)
+{
+	int ret;
+	int i = 0;
+	char name[20];
+	assert(pc);
+	if (pc->count == 0)
+	{
+		printf("通讯录为空，无法删除\n");
+		return;
+	}
+	printf("请输入姓名:>");
+	scanf("%s", name);
+	ret=FindByName(pc,name);
+	if (ret > 0)
+	{
+		for (i = ret;i < pc->count - 1;i++)
+		{
+			pc->data[i] = pc->data[i + 1];
+		}
+		(pc->count)--;
+		printf("删除成功\n");
+	}
+	else
+	{
+		printf("未找到此联系人\n");
+	}
+}
+
+void SearchContact(const Contact* pc)
+{
+	char name[20];
+	int ret;
+	assert(pc);
+	if (pc->count == 0)
+	{
+		printf("通讯录为空，无法查询\n");
+		return;
+	}
+	printf("请输入姓名:>");
+	scanf("%s", name);
+	ret = FindByName(pc, name);
+	if (ret > 0)
+	{
+		printf("%-20s\t%-3s\t%-5s\t%-12s\t%-30s\n", "姓名", "年龄", "性别", "电话", "地址");
+
+		printf("%-20s\t%-3d\t%-5s\t%-12s\t%-30s\n", pc->data[ret].name,
+			pc->data[ret].age,
+			pc->data[ret].sex,
+			pc->data[ret].tele,
+			pc->data[ret].addr);
+		printf("查询成功\n");
+	}
+	else
+	{
+		printf("未找到此联系人\n");
+	}
+}
+void ModifyhContact(const Contact* pc)
+{
+	char name[20];
+	int ret;
+	assert(pc);
+	if (pc->count == 0)
+	{
+		printf("通讯录为空，无法修改\n");
+		return;
+	}
+	printf("请输入被修改人的姓名:>");
+	scanf("%s", name);
+	ret = FindByName(pc, name);
+	if (ret > 0)
+	{
+		printf("请输入新的姓名:>");
+		scanf("%s", pc->data[ret].name);
+		printf("请输入年龄:>");
+		scanf("%d", &(pc->data[ret].age));
+		printf("请输入性别:>");
+		scanf("%s", pc->data[ret].sex);
+		printf("请输入电话:>");
+		scanf("%s", pc->data[ret].tele);
+		printf("请输入地址:>");
+		scanf("%s", pc->data[ret].addr);
+		printf("修改成功\n");
+	}
+	else
+	{
+		printf("未找到此联系人\n");
+	}
+}
+int cmp_peo_by_name(const void* e1, const void* e2)
+{
+	return(strcmp(((PeoInfo*)e1)->name, ((PeoInfo*)e2)->name));
+}
+void SortContact(const Contact* pc)
+{
+	assert(pc);
+	qsort(pc->data, pc->count, sizeof(PeoInfo), cmp_peo_by_name);
+	printf("排序成功\n");
+}
---
+++ 3_28/contact.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include<string.h>
+#include<assert.h>
+#include <stdio.h>
+#include<stdlib.h>
+//类型声明
+typedef struct PeoInfo
+{
+	char name[20];
+	int age;
+	char sex[10];
+	char tele[12];
+	char addr[30];
+}PeoInfo;
+
+
+//通讯录
+typedef struct Contact
+{
+	PeoInfo data[100];
+	int count;
+}Contact;
+
+void InitContact(Contact *pc);//初始化
+//增加成员
+void AddContact(Contact* pc);
+//删除成员
+void DelContact(Contact* pc);
+
+//展示通讯录
+void ShowContact(const Contact* pc);
+
+//查询通讯录
+void SearchContact(const Contact* pc);
+
+//修改通讯录
+void ModifyhContact(const Contact* pc);
+//给通讯录排序
+void SortContact(const Contact* pc);
